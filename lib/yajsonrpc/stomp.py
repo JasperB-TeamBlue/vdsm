f4ad82b4aa751b53cc0d8d7da6c4ec53cb70d5ef
Jasper Berton <jasper.berton@team.blue>
2026-06-09 14:14:52 +0200

    Refactor due to splicing syntax error
    Black likes to improve readability and add a space before : in splicing when the splice element is an operation instead of a straight result.
    Pylint on the other hand does not like whitespace before a :.
    To solve this clash, extract the operation to a variable and use that variable in the splice.
    
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- lib/yajsonrpc/stomp.py
+++ lib/yajsonrpc/stomp.py
@@ -296,7 +296,8 @@ class Parser(object):
             raise RuntimeError("Frame doesn't end with NULL byte")
 
         self._flush()
-        self._write_buffer(buf[cl + 1 :])
+        cl_end = cl + 1
+        self._write_buffer(buf[cl_end:])
         body = buf[:cl]
 
         self._tmp_frame.body = body
